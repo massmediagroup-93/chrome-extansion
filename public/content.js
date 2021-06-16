541cf0d2ca11013ebcfa450f39fd920db963b09f
Yaroslav Kovalchuk <mmg.kovalchuk.yaroslav@gmail.com>
2021-06-16 17:04:08 +0300

    add show\hide app on extansion btn click

--- public/content.js
+++ public/content.js
@@ -9,17 +9,27 @@ function main() {
   const extensionOrigin = `chrome-extension://${chrome.runtime.id}`
   // eslint-disable-next-line no-restricted-globals
   if (!location.ancestorOrigins.contains(extensionOrigin)) {
-    // Fetch the local React index.html page
-    // eslint-disable-next-line no-undef
-    fetch(chrome.runtime.getURL('index.html') /* , options */)
-      .then(response => response.text())
-      .then(html => {
-        const styleStashHTML = html.replace(/\/static\//g, `${extensionOrigin}/static/`)
-        // eslint-disable-next-line no-undef
-        $(styleStashHTML).appendTo('body')
-      })
-      .catch(error => {
-        console.warn(error)
-      })
+    const testChromeExtension = document.getElementById('test-chrome-extension')
+
+    if (testChromeExtension) {
+      if (testChromeExtension.style.display === 'none') {
+        testChromeExtension.style.display = 'block'
+      } else {
+        testChromeExtension.style.display = 'none'
+      }
+    } else {
+      // Fetch the local React index.html page
+      // eslint-disable-next-line no-undef
+      fetch(chrome.runtime.getURL('index.html') /* , options */)
+        .then(response => response.text())
+        .then(html => {
+          const styleStashHTML = html.replace(/\/static\//g, `${extensionOrigin}/static/`)
+          // eslint-disable-next-line no-undef
+          $(styleStashHTML).appendTo('body')
+        })
+        .catch(error => {
+          console.warn(error)
+        })
+    }
   }
 }
